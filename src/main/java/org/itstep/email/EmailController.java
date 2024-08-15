e0b137c900cc7c4585ea9cad5daa5a883e1655cb
gk <demar.demidov@yandex.ru>
2024-08-15 21:23:04 +0300

    test

--- src/main/java/org/itstep/email/EmailController.java
+++ src/main/java/org/itstep/email/EmailController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class EmailController {
     @Autowired
@@ -14,9 +17,11 @@ public class EmailController {
     @GetMapping("/sendsimple")
     public String sendSimple(Model model){
         Email email = new Email();
-        email.setTo("demar.demidov@yandex.ru");
-        email.setFrom("demar.demidov@yandex.ru");
+        email.setTo("prizrak220V.AD@gmail.com");
+        email.setFrom("prizrak220V.AD@gmail.com");
         email.setSubject("Simple text");
+        email.setText("Simple text");
+        Map<String, Object> properties = new HashMap<>();
         emailService.sendSimpleMessage(email);
         return "success";
     }
